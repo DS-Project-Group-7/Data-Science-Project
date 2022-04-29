6e36f5dbe89a2b2a666bae7c3c2efe0b38c12295
samsnog <samsnog@hotmail.fr>
2022-04-29 15:26:22 +1000

    Output only has selected columns
    Added dictionaries for the columns we want to output.
    
    The output now only contains the selected columns and not the original ones.
    
    Added a function to treat boolean features (boxes that would either be ticked or not in the filemakerpro file).
    
    Only needs to select the columns we want in the dictionnary.

--- src/dataCleaning.py
+++ src/dataCleaning.py
@@ -1,59 +1,94 @@
+from calendar import c
 import pandas as pd
 import argparse
 
+# Definition of Dictionaries that link a wanted new feature to the index of the the ones they are computed from
+# Eg. New feature "Support Type" from CategoricalColumnsDict will refer to columns 9 and 13 of the originial excel data file.
+BooleanColumnsDict={
+"Ground Layer to side edge": 216,
+"Ground Layer to face edge": 217
+}
+
+CategoricalColumnsDict={
+"Support Type": [9,13],
+"Support originality": [138,139],
+"New support type": [140,141],
+}
+
+OrdinalColumnsDict={
+"Auxiliary support Condition":[23,24,25],
+"Media (Paint layer) Condition": [58,59,60, 61],
+"Painting Support Condition":[41,42,43]
+}
+
+
 def main(dataFile):
-    df = pd.read_excel(dataFile)
+    cleanDataDf = pd.DataFrame()
+    originalDataDf = pd.read_excel(dataFile)
 
-    fuseCategColumns(df,[9,13],"Support Type")
-    fuseCategColumns(df, [138,139], "Support originality")
-    fuseCategColumns(df, [140,141], "New support type")
-    fuseCategColumns(df, [216,217], "Ground Layer to side or face edge")
+    for feature in CategoricalColumnsDict:
+        fuseCategColumns(originalDataDf, cleanDataDf, CategoricalColumnsDict[feature], feature)
 
-    fuseOrdinalColumns(df,[23,24,25], "Auxiliary support Condition")
-    fuseOrdinalColumns(df,[58,59,60, 61], "Media (Paint layer) Condition")
-    fuseOrdinalColumns(df,[41,42,43], "Painting Support Condition")
+    for feature in OrdinalColumnsDict:
+        fuseOrdinalColumns(originalDataDf,cleanDataDf, OrdinalColumnsDict[feature], feature)
+
+    for feature in BooleanColumnsDict:
+        makeBoolCol(originalDataDf, cleanDataDf, BooleanColumnsDict[feature], feature)
 
     #The media ground layer quality has multiple values.
     #Ground layer to side or face edge also has a record with multiple values
-    df.to_csv('../data/rawDataPlusCleanColumns.csv')
+    cleanDataDf.to_csv('../data/cleanData.csv')
     print("done")
 
-def fuseCategColumns(df,indexList, colName):
+
+def makeBoolCol(originalDf,cleanDf, index, colName):
+    """ Create a proper boolean feature from a column which values would either be present or absent
+    originalDf : pandas dataframe of original data
+    cleanDf : pandas dataframe of cleansed columns
+    index : index of the column
+    colName: name of the fused column
+    """
+    cleanDf[colName] = (~originalDf.iloc[:,index].isnull()).astype(int)
+    
+
+def fuseCategColumns(originalDf,cleanDf ,indexList, colName):
     """Fuse an array of columns in a dataframe that show diferent aspects of the same categorical variable
-    df : pandas dataframe object
+    originalDf : pandas dataframe of original data
+    cleanDf : pandas dataframe of cleansed columns
     indexList : list of the indexes of the columns we want to fuse
     colName: name of the fused column
     """
 
     #Replace Nans with an empty string -> might want a method that parses the df and does an appropriate filling according to the categorical or numerical value of the column
     for index in indexList:
-        df.iloc[:,index]=df.iloc[:,index].fillna('')
+        originalDf.iloc[:,index]=originalDf.iloc[:,index].fillna('')
 
     #Since columns should be aligned (when a columns is not empty, all the others shuld be), we can just add them up
     #Might want to add a verification part though
-    df[colName] = df.iloc[:,indexList[0]]
+    cleanDf[colName] = originalDf.iloc[:,indexList[0]]
     for index in indexList[1:]:
-        df[colName]+= df.iloc[:,index]
+        cleanDf[colName]+= originalDf.iloc[:,index]
 
-def fuseOrdinalColumns(df,orderedIndexList, colName):
+def fuseOrdinalColumns(originalDf,cleanDf,orderedIndexList, colName):
     """Fuse an array of columns in a dataframe that show diferent levels of the same categorical variable
-    df : pandas dataframe object
+    originalDf : pandas dataframe of original data
+    cleanDf : pandas dataframe of cleansed columns
     indexList : list of the indexes of the columns we want to fuse, from the lower level to the highest
     colName: name of the fused column
     """
 
     #Replace Nans with an empty string -> might want a method that parses the df and does an appropriate filling according to the categorical, ordinal or numerical value of the column
     for index in orderedIndexList:
-        df.iloc[:,index]=df.iloc[:,index].fillna(0)
+        originalDf.iloc[:,index]=originalDf.iloc[:,index].fillna(0)
 
     #Since columns should be aligned (when a columns is not empty, all the others shuld be), we can just add them up
     #Might want to add a verification part though
     level=0
-    df[colName] = level #set the default value to 0
+    cleanDf[colName] = level #set the default value to 0
 
     for index in orderedIndexList[1:]:
         level+=1
-        df.loc[df.iloc[:,index]!=0, colName]=level
+        cleanDf.loc[originalDf.iloc[:,index]!=0, colName]=level
 
 def parseArguments():
     parser = argparse.ArgumentParser()
