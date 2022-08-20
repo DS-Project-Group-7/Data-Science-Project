ee6a5929636328fd37e850afb5046a062d6fcf83
supAmorn <supanuthaa@gmail.com>
2022-08-20 12:03:20 +1000

    Update tooltips for plotting
    Dimension, Aux support tab

--- SEA_Oil_Painting_Dashboard/server.R
+++ SEA_Oil_Painting_Dashboard/server.R
@@ -32,11 +32,13 @@ shinyServer(function(input, output) {
       select(collection, length,width,title) %>%
       hchart("scatter", 
              hcaes(x = width, y = length, group = collection)) %>%
-      hc_tooltip(crosshairs = TRUE, shared = TRUE) %>%
+      #hc_tooltip(crosshairs = TRUE, shared = TRUE) %>%
       hc_xAxis(title = list(text = "Width")) %>%
       hc_yAxis(title = list(text = "Length")) %>%
-      hc_title(text = "Scatter plot between width and length of four collections")
-      #Need to update tooltips
+      hc_title(text = "Scatter plot between width and length of four collections")%>%
+      hc_tooltip(pointFormat = tooltip_table(c("Painting Title:","Width:", "Length:"), 
+                                            c("{point.title}", "{point.x}","{point.y}")), useHTML = TRUE)
+    #Need to update tooltips
   })
   
   
@@ -65,16 +67,21 @@ shinyServer(function(input, output) {
       count(auxiliary_support_condition, collection) %>%
       hchart("bar", stacking = "normal",
              hcaes(x = collection, y = n, group = auxiliary_support_condition)) %>%
-      hc_tooltip(crosshairs = TRUE, shared = TRUE) %>%
       hc_xAxis(title = list(text = "Museum")) %>%
       hc_yAxis(title = list(text = "Number of Paintings")) %>%
-      hc_title(text = "Auxiliary support condition")
-      #Need to update tooltips
+      hc_legend(title = list(text = "Condition Score"), reversed = TRUE) %>%
+      hc_title(text = "Auxiliary support condition")%>%
+      hc_tooltip(pointFormat = tooltip_table(c("Auxiliary support condition:", "Number of paintings:"), 
+                                             c("{point.auxiliary_support_condition}", "{point.y}")), useHTML = TRUE)
   })
   
   #Heat map aux condition
   output$AX_heat <- renderHighchart({
-    art %>% group_by(!!sym(input$AX)) %>%
+    art %>% 
+      mutate(!!sym(input$AX) := recode(!!sym(input$AX), "0" = "0 No", "1" = "1 Yes"),
+             auxiliary_support_condition = 
+               recode(auxiliary_support_condition, "0" = "0 Poor", "1" = "1 Fair", "2" = "2 Good"))%>% 
+      group_by(!!sym(input$AX)) %>%
       filter(between(decade, input$AX_decade[1], input$AX_decade[2])) %>%
       count(condition = auxiliary_support_condition) %>%
       hchart("heatmap",
@@ -84,8 +91,10 @@ shinyServer(function(input, output) {
                categories = c("Poor", "Fair", "Good" )) %>%
       hc_yAxis(title = list(text = names(AX_choiceVec)[AX_choiceVec == input$AX]),
                categories = c("No", "Yes")) %>%
-      hc_title(text = "Heatmap Auxiliary support condition")
-      #Need to update tooltips and dynamic title
+      hc_title(text = paste0("Heatmap ",names(AX_choiceVec)[AX_choiceVec == input$AX]," Condition"))%>%
+      hc_tooltip(pointFormat = tooltip_table(c(names(AX_choiceVec)[AX_choiceVec == input$AX],"Auxiliary support condition:", "Number of paintings:"), 
+                                             c("{point.y}","{point.condition}", "{point.n}")), useHTML = TRUE)
+      #Need to update tooltips 
   })
   
   #Bar chart options painting condition
