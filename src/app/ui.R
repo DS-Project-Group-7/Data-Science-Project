02c99bd97f4d5e311d6a79af47e40d8ca6e48b9a
supAmorn <supanuthaa@gmail.com>
2022-08-22 20:08:18 +1000

    Revert "Merge branch 'main' of https://github.com/DS-Project-Group-7/Data-Science-Project"
    This reverts commit 0a4d6915244a527f13773786b048fe5d2f9c5db3, reversing
    changes made to f240a9b85fba30391ce6f69b35c005033c348147.

--- src/app/ui.R
+++ src/app/ui.R
@@ -82,7 +82,7 @@ body <- dashboardBody(
                                     sliderInput("PS_decade", "Select a time period for visualisation",
                                                 min = 1850, max = 1970, step = 10, value = c(1850, 1970))
                                   ),
-                                  mainPanel(highchartOutput("PS_visual"))
+                                  mainPanel(highchartOutput("PS_planar"))
                                 ),
                                 sidebarLayout(
                                   sidebarPanel(
@@ -96,22 +96,7 @@ body <- dashboardBody(
                                   )
                                 )
                        ),
-                       tabPanel("Ground Layer",
-                                fluidRow(
-                                  column(12, highchartOutput("GR_eval"))
-                                ),
-                                sidebarLayout(
-                                  sidebarPanel(
-                                    selectInput("GR", "Choose a ground layer condition to view a brief summary:",
-                                                GR_choiceVec),
-                                    sliderInput("GR_decade", "Select a time period for visualisation",
-                                                min = 1850, max = 1970, step = 10, value = c(1850, 1970))
-                                  ),
-                                  mainPanel(
-                                    highchartOutput("GR_visual")
-                                  )
-                                )
-                       ),
+                       tabPanel("Ground Layer"),
                        tabPanel("Paint Layer"),
                        tabPanel("Frame"))
     ),
