434c7a11c544350d1edd5432c3b6cc6a63e53b19
haonan99 <616012788@qq.com>
2022-09-05 18:23:41 +1000

    Added filter for museum in the plot

--- SEA_Oil_Painting_Dashboard/server.R
+++ SEA_Oil_Painting_Dashboard/server.R
@@ -206,6 +206,7 @@ shinyServer(function(input, output) {
     art %>% 
       mutate(painting_support_condition = 
                recode(painting_support_condition, "0" = "0 Poor", "1" = "1 Fair", "2" = "2 Good", "3" = "3 Excellent")) %>%
+      filter(collection %in% input$PS_check) %>% 
       filter(between(decade, input$PS_decade[1], input$PS_decade[2])) %>%
       count(painting_support_condition, collection) %>%
       hchart("bar", stacking = "normal",
@@ -220,6 +221,7 @@ shinyServer(function(input, output) {
   output$PS_visual <- renderHighchart({
     art %>% 
       mutate(!!sym(input$PS) := recode(!!sym(input$PS), "0" = "0 No", "1" = "1 Yes")) %>%
+      filter(collection %in% input$PS_check) %>% 
       filter(between(decade, input$PS_decade[1], input$PS_decade[2])) %>%
       count(!!sym(input$PS), collection) %>%
       hchart("bar", stacking = "normal",
@@ -234,8 +236,10 @@ shinyServer(function(input, output) {
   
   output$PS_heatmap <- renderHighchart({
     art %>%
+      filter(collection %in% input$PS_check) %>% 
       mutate(!!sym(input$PS_1) := recode(!!sym(input$PS_1), "0" = "0 No", "1" = "1 Yes")) %>%
       mutate(!!sym(input$PS_2) := recode(!!sym(input$PS_2), "0" = "0 No", "1" = "1 Yes")) %>%
+      filter(collection %in% input$PS_check) %>% 
       count(!!sym(input$PS_1), !!sym(input$PS_2)) %>%
       hchart("heatmap", hcaes(x = !!sym(input$PS_1), y = !!sym(input$PS_2), value = n)) %>%
       hc_xAxis(title = list(text = names(PS_choiceVec)[PS_choiceVec == input$PS_1])) %>%
